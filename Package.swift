26be9cfe5ce61e0ed6a7f98f2493c9cb3ff127b1
Joe McBride <255007+joemcbride@users.noreply.github.com>
2021-12-04 15:27:42 -0800

    Rename

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Outlander",
+    name: "Outlander Plugins",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Outlander",
-            targets: ["Outlander"]),
+            name: "OutlanderPlugin",
+            targets: ["OutlanderPlugin"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Outlander",
+            name: "OutlanderPlugin",
             dependencies: []),
         .testTarget(
-            name: "OutlanderTests",
-            dependencies: ["Outlander"]),
+            name: "OutlanderPluginTests",
+            dependencies: ["OutlanderPlugin"]),
     ]
 )
