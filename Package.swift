5d3fcb33d743802ea1fc86bad39c1f2a1fe4a7e1
Joe McBride <255007+joemcbride@users.noreply.github.com>
2021-12-04 19:28:02 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "OutlanderPlugin",
-            targets: ["OutlanderPlugin"]),
+            name: "Plugins",
+            targets: ["Plugins"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -17,7 +17,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "OutlanderPlugin",
+            name: "Plugins",
             url: "https://github.com/outlander-app/plugin-package/releases/download/v1.0.0/Plugins.xcframework.zip",
             checksum: "70ae56a510f384cd03f599f83df58653319712c447b93ae539661b1ab64fc943"
         )
