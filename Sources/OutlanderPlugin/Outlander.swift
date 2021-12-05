5864fc28080fcbf37de1bcebf4347f5db45e0623
Joe McBride <255007+joemcbride@users.noreply.github.com>
2021-12-04 17:10:05 -0800

    Add base plugin class

--- Sources/OutlanderPlugin/Outlander.swift
+++ Sources/OutlanderPlugin/Outlander.swift
@@ -1,3 +1,5 @@
+import Cocoa
+
 public protocol IHost {
     func send(text: String)
     func get(variable: String) -> String
@@ -6,11 +8,32 @@ public protocol IHost {
 }
 
 public protocol OPlugin {
-    init()
     var name: String { get }
     func initialize(host: IHost)
     func variableChanged(variable: String, value: String)
     func parse(input: String) -> String
     func parse(xml: String) -> String
     func parse(text: String) -> String
+}
+
+@objc public class Plugin: NSObject, OPlugin {
+    public var name: String = ""
+
+    public func initialize(host: IHost) {
+    }
+
+    public func variableChanged(variable: String, value: String) {
+    }
+    
+    public func parse(input: String) -> String {
+        return input
+    }
+    
+    public func parse(xml: String) -> String {
+        return xml
+    }
+    
+    public func parse(text: String) -> String {
+        return text
+    }
 }
